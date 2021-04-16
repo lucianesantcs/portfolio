1a925ae41107b5df63ebc8cfde5b40c83aa22486
Luciane Santos <lucianesantcs@gmail.com>
2021-04-16 17:44:15 -0300

    modificado css dos componentes para module css

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import * as FiIcons from "react-icons/fi";
 
-import "./About.css";
+import styles from "./About.module.css";
 import icon from "../assets/icon.jpeg";
 import Head from "../Components/Head";
 
@@ -9,12 +9,12 @@ function About() {
   return (
     <>
       <Head title="Sobre" description="Desenvolvedora front-end e UI Designer" />
-      <main className="about animeLeft">
-        <div className="about-content">
-          <div className="about-content-item">
-            <img src={icon} alt="" className="about-image" />
+      <main className={`${styles.about} animeLeft`}>
+        <div className={styles.aboutContent}>
+          <div className={styles.aboutContentItem}>
+            <img src={icon} alt="" className={styles.aboutImage} />
           </div>
-          <div className="about-content-item">
+          <div className={styles.aboutContentItem}>
             <h1>Luciane Santos</h1>
             <h3>
               Front-End developer & {"\n"}
@@ -23,16 +23,17 @@ function About() {
             <p>
               6+ anos sendo autodidata no mundo da programação começando com os pilares do Front-End
               com as linguagens HTML e CSS, um pouco mais tarde o Javascript. Possuo conhecimento em{" "}
-              <u className="underline">HTML5 e CSS3</u>. No momento estou estudando{" "}
-              <u className="underline">ReactJS</u> e procurando me aprofundar mais na linguagem{" "}
-              <u className="underline">Javascript</u>, sendo meu foco nela e no front-end, mas tenho
-              interesse em <u className="underline">UI Design</u> e uso{" "}
-              <u className="underline">Figma</u> para criação de interfaces. Também tenho
-              familiaridade com <u className="underline">Adobe Photoshop</u> para edição de imagens.
+              <u className={styles.underline}>HTML5 e CSS3</u>. No momento estou estudando{" "}
+              <u className={styles.underline}>ReactJS</u> e procurando me aprofundar mais na
+              linguagem <u className={styles.underline}>Javascript</u>, sendo meu foco nela e no
+              front-end, mas tenho interesse em <u className={styles.underline}>UI Design</u> e uso{" "}
+              <u className={styles.underline}>Figma</u> para criação de interfaces. Também tenho
+              familiaridade com <u className={styles.underline}>Adobe Photoshop</u> para edição de
+              imagens.
             </p>
           </div>
-          <nav className="about-content-item">
-            <ul className="about-social">
+          <nav className={styles.aboutContentItem}>
+            <ul className={styles.aboutSocial}>
               <li data-tooltip="Github">
                 <a href="https://github.com/lucianesantcs" target="_blank" rel="noreferrer">
                   <FiIcons.FiGithub />
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -2,20 +2,20 @@ import React from "react";
 import * as FiIcons from "react-icons/fi";
 import Head from "../Components/Head";
 
-import "./Contact.css";
+import styles from "./Contact.module.css";
 
 const Contact = () => {
   return (
     <>
       <Head title="Contato" description="Onde me encontrar" />
-      <section className="contact animeLeft">
-        <div className="contact-content">
-          <div className="contact-content-item">
+      <section className={`${styles.contact} animeLeft`}>
+        <div className={styles.contactContent}>
+          <div className={styles.contactContentItem}>
             <h2>Onde me encontrar</h2>
             <p>
               Entre em contato através das minhas redes sociais ou enviando um e-mail para{" "}
               <a href="mailto:lucianesantcs@gmail.com">
-                <u className="underline">
+                <u className={styles.underline}>
                   <strong>lucianesantcs@gmail.com</strong>
                 </u>
               </a>{" "}
@@ -23,9 +23,9 @@ const Contact = () => {
               pequenos, colaborações, ou até mesmo se precisar de ajuda com algum projeto.
             </p>
           </div>
-          <div className="contact-content-item-horizontal-divider"></div>
-          <nav className="contact-content-item">
-            <ul className="contact-social">
+          <div className={styles.contactContentItemHorizontalDivider}></div>
+          <nav className={styles.contactContentItem}>
+            <ul className={styles.contactSocial}>
               <li>
                 <FiIcons.FiGithub />
                 <a href="https://github.com/lucianesantcs" target="_blank" rel="noreferrer">
